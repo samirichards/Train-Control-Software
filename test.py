793892c737c0e53d27327dd0d30d10b14d782de8
Sami Richards <1902765@uad.ac.uk>
2023-02-09 16:36:34 +0000

    Update test.py
    Making sure the pigpio library works

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import time
+import pigpio
+
+LED_PIN = 37
+
+gpio = pigpio.pi
+gpio.set_mode(LED_PIN, pigpio.OUTPUT)
+gpio.set_pull_up_down(LED_PIN, pigpio.PUD_UP)
+while(True):
+    gpio.write(LED_PIN, pigpio.HIGH)
+    print("Pin 37 is: " + gpio.read(LED_PIN))
+    time.sleep(1)
+    gpio.write(LED_PIN, pigpio.LOW)
+    print("Pin 37 is: " + gpio.read(LED_PIN))
+    time.sleep(1)
