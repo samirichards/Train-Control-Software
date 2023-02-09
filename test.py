61d4bfcd3e2690d0dba353de7d6e5ff88e567ea5
Sami Richards <1902765@uad.ac.uk>
2023-02-09 16:54:48 +0000

    asdasdasd

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import time
 import pigpio
 
-LED_PIN = 37
+LED_PIN = 26
 
 gpio = pigpio.pi()
 gpio.set_mode(LED_PIN, pigpio.OUTPUT)
 gpio.set_pull_up_down(LED_PIN, pigpio.PUD_UP)
 while(True):
     gpio.write(LED_PIN, pigpio.HIGH)
-    print("Pin 37 is: " + gpio.read(LED_PIN))
+    print("Pin 26 is: " + gpio.read(LED_PIN))
     time.sleep(1)
     gpio.write(LED_PIN, pigpio.LOW)
-    print("Pin 37 is: " + gpio.read(LED_PIN))
+    print("Pin 26 is: " + gpio.read(LED_PIN))
     time.sleep(1)
